7b713f78d755258056f71478f58f6948264fa20c
Reyel <reyel.conceicao@bandtec.com.br>
2022-10-11 14:25:16 -0300

    Como doar

---
+++ aplicativo/app/src/main/java/com/example/pluslife/ComoDoarActivity.kt
@@ -0,0 +1,11 @@
+package com.example.pluslife
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ComoDoarActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_como_doar)
+    }
+}
