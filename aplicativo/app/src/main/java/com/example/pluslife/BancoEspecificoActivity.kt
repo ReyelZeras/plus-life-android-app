c40f9c27464107b1e154d88ca3652c837eba2f6b
Brunozapa <zapabrunosilva@gmail.com>
2022-11-11 20:33:28 -0300

    Banco especifico

--- aplicativo/app/src/main/java/com/example/pluslife/BancoEspecificoActivity.kt
+++ aplicativo/app/src/main/java/com/example/pluslife/BancoEspecificoActivity.kt
@@ -7,6 +7,10 @@ import android.os.Bundle
 import com.example.pluslife.databinding.ActivityBancoEspecificoBinding
 import com.example.pluslife.databinding.ActivityBuscarEnderecoBinding
 import com.example.pluslife.models.NomeNivelSang
+import com.example.pluslife.models.enum.MedidorNivel
+import com.example.pluslife.models.enum.MedidorNivel.ATENCAO
+import com.example.pluslife.models.enum.MedidorNivel.CRITICO
+import com.example.pluslife.models.enum.MedidorNivel.ESTAVEL
 
 class BancoEspecificoActivity : AppCompatActivity() {
 
@@ -27,12 +31,13 @@ class BancoEspecificoActivity : AppCompatActivity() {
         val email = intent.getStringExtra("email")
         val telefone = intent.getStringExtra("telefone")
         val endereco = intent.getStringExtra("endereco")
-        var estoque: ArrayList<NomeNivelSang> = intent.getSerializableExtra("estoque")  as ArrayList<NomeNivelSang>
+        val estoque: ArrayList<NomeNivelSang> = intent.getSerializableExtra("estoque")  as ArrayList<NomeNivelSang>
 
         binding.tvNome.text = nome
         binding.tvEmail.text = email
         binding.tvTelefone.text = telefone
         binding.tvEndereco.text= endereco
+        definirEstoque(estoque)
 
 
     }
@@ -50,12 +55,50 @@ class BancoEspecificoActivity : AppCompatActivity() {
         startActivity(novaTela)
     }
 
-    fun definirEstoque(estoque: ArrayList<NomeNivelSang>){
-
-
-
-    
-
-
+    fun definirEstoque(listaEstoque: ArrayList<NomeNivelSang>){
+        for (estoque in listaEstoque) {
+            when (estoque.nome) {
+                "A+" -> {
+                    if (estoque.nivel === ESTAVEL.nivel) binding.medidorAp.setImageResource(R.drawable.medidor_ap_estavel)
+                    if (estoque.nivel === ATENCAO.nivel) binding.medidorAp.setImageResource(R.drawable.medidor_ap_atencao)
+                    if (estoque.nivel === CRITICO.nivel) binding.medidorAp.setImageResource(R.drawable.medidor_ap_critico)
+                }
+                "A-" -> {
+                    if (estoque.nivel === ESTAVEL.nivel) binding.medidorAn.setImageResource(R.drawable.medidor_an_estavel)
+                    if (estoque.nivel === ATENCAO.nivel) binding.medidorAn.setImageResource(R.drawable.medidor_an_atencao)
+                    if (estoque.nivel === CRITICO.nivel) binding.medidorAn.setImageResource(R.drawable.medidor_an_critico)
+                }
+                "B+" -> {
+                    if (estoque.nivel === ESTAVEL.nivel) binding.medidorBp.setImageResource(R.drawable.medidor_bp_estavel)
+                    if (estoque.nivel === ATENCAO.nivel) binding.medidorBp.setImageResource(R.drawable.medidor_bp_atencao)
+                    if (estoque.nivel === CRITICO.nivel) binding.medidorBp.setImageResource(R.drawable.medidor_bp_critico)
+                }
+                "B-" -> {
+                    if (estoque.nivel === ESTAVEL.nivel) binding.medidorBn.setImageResource(R.drawable.medidor_bn_estavel)
+                    if (estoque.nivel === ATENCAO.nivel) binding.medidorBn.setImageResource(R.drawable.medidor_bn_atencao)
+                    if (estoque.nivel === CRITICO.nivel) binding.medidorBn.setImageResource(R.drawable.medidor_bn_critico)
+                }
+                "AB+" -> {
+                    if (estoque.nivel === ESTAVEL.nivel) binding.medidorAbp.setImageResource(R.drawable.medidor_abp_estavel)
+                    if (estoque.nivel === ATENCAO.nivel) binding.medidorAbp.setImageResource(R.drawable.medidor_abp_atencao)
+                    if (estoque.nivel === CRITICO.nivel) binding.medidorAbp.setImageResource(R.drawable.medidor_abp_critico)
+                }
+                "AB-" -> {
+                    if (estoque.nivel === ESTAVEL.nivel) binding.medidorAbn.setImageResource(R.drawable.medidor_abn_estavel)
+                    if (estoque.nivel === ATENCAO.nivel) binding.medidorAbn.setImageResource(R.drawable.medidor_abn_atencao)
+                    if (estoque.nivel === CRITICO.nivel) binding.medidorAbn.setImageResource(R.drawable.medidor_abn_critico)
+                }
+                "O+" -> {
+                    if (estoque.nivel === ESTAVEL.nivel) binding.medidorOp.setImageResource(R.drawable.medidor_op_estavel)
+                    if (estoque.nivel === ATENCAO.nivel) binding.medidorOp.setImageResource(R.drawable.medidor_op_atencao)
+                    if (estoque.nivel === CRITICO.nivel) binding.medidorOp.setImageResource(R.drawable.medidor_op_critico)
+                }
+                "O-" -> {
+                    if (estoque.nivel === ESTAVEL.nivel) binding.medidorOn.setImageResource(R.drawable.medidor_on_estavel)
+                    if (estoque.nivel === ATENCAO.nivel) binding.medidorOn.setImageResource(R.drawable.medidor_on_atencao)
+                    if (estoque.nivel === CRITICO.nivel) binding.medidorOn.setImageResource(R.drawable.medidor_on_critico)
+                }
+            }
+        }
     }
 }
---
+++ aplicativo/app/src/main/java/com/example/pluslife/models/enum/MedidorNivel.kt
@@ -0,0 +1,7 @@
+package com.example.pluslife.models.enum
+
+enum class MedidorNivel(val nivel: String) {
+    ESTAVEL("Estável"),
+    CRITICO("Crítico"),
+    ATENCAO("Atenção")
+}
