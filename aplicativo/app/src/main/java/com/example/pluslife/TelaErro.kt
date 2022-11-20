00f51f7bb911a5c071e7cb1be7db19498806e18b
Andre-K-G <andre140894@gmail.com>
2022-11-20 07:30:33 -0300

    upload de telas sucesso e erro

---
+++ aplicativo/app/src/main/java/com/example/pluslife/TelaErro.kt
@@ -0,0 +1,11 @@
+package com.example.pluslife
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TelaErro : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tela_erro)
+    }
+}
